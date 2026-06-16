ce05b39fa53c9a7d40c9d3f8a7a85bc15a8ff809
Pranav09130 <pranavmedhe15@gmail.com>
2026-06-16 15:16:11 +0530

    fix db env variable names

--- task4-5-backend/db.js
+++ task4-5-backend/db.js
@@ -5,11 +5,11 @@ let db;
 async function getDb() {
   if (!db) {
     db = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      host: process.env.MYSQLHOST,
+      port: process.env.MYSQLPORT,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE
     });
   }
   return db;
